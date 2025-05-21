060fa6b686dc503dec6a7b6f9f0ddbf7173cc3e7
SRISHAKTHI S R <132913726+srishakthi04@users.noreply.github.com>
2025-05-21 15:52:57 +0530

    Create word.java

---
+++ word.java
@@ -0,0 +1,23 @@
+class Word{
+String word;
+String type;
+public word(String word,String type){
+this.word=word;
+this.type=type;
+}
+public String getWord(){
+return word;
+}
+public String getType(){
+return type;
+}
+public void setWord(String word){
+this.word=word;
+}
+public void setType(String type){
+this.type=type;
+}
+public String toString(){
+return "Word: "+word+" Type:"+type'
+}
+}
